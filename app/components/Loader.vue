e67da79b5682e65edf3c866605f686369a780f40
Andrey Dumskiy <dumskiy-25@mail.ru>
2026-05-15 12:37:08 +0300

    Added Loader

---
+++ app/components/Loader.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="loader">
+    <div class="spinner"></div>
+    <p>Loading...</p>
+  </div>
+</template>
+
+<style scoped>
+.loader {
+  text-align: center;
+  padding: 3rem;
+  font-size: 2rem;
+}
+
+.spinner {
+  width: 7rem;
+  height: 7rem;
+  border: 0.5rem solid var(--color-gray-secondary);
+  border-top-color: var(--color-purple-secondary);
+  border-radius: 50%;
+  animation: spin 1s linear infinite;
+  margin: 0 auto 1rem;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
